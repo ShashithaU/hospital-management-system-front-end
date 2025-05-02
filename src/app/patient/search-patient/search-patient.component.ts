a2da45a2a56fa03127b87f6ee0507ce1221ad676
ShashithaU <udeshshashitha@gmail.com>
2025-05-02 08:59:17 +0530

    patient search implemented

--- src/app/patient/search-patient/search-patient.component.ts
+++ src/app/patient/search-patient/search-patient.component.ts
@@ -2,41 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Observable, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { Patient } from '../../../models/patient.model';
 @Component({
   selector: 'app-search-patient',
   standalone: true,
-  imports: [MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-  CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-patient.component.html',
-  styleUrl: './search-patient.component.css'
+  styleUrl: './search-patient.component.css',
 })
-export class SearchPatientComponent implements OnInit {
-  searchControl = new FormControl();
-  filteredPatients!: Observable<any[]>;
-  apiUrl = 'http://localhost:8080/patient/search-by-name/'; // Update with your API URL
+export class SearchPatientComponent {
+  searchTerm = '';
+  filteredPatients: any[] = [];
+  apiUrl = 'http://localhost:8080/patient/search-by-name/';
+  selectedPatient: number = 0;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.filteredPatients = this.searchControl.valueChanges.pipe(
-      debounceTime(300), // wait 300ms after each keystroke
-      distinctUntilChanged(), // ignore if same as previous value
-      switchMap(term => term ? this.searchPatients(term) : [])
-    );
+  onSearch() {
+    if (this.searchTerm.length === 0) {
+      this.filteredPatients = [];
+      return;
+    }
+    this.http
+      .get<any>(`${this.apiUrl}${this.searchTerm}`)
+      .subscribe((data) => (this.filteredPatients = data));
+    console.log(this.filteredPatients);
   }
 
-  searchPatients(term: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}${term}`);
-  }
-
-  displayFn(patient: any): string {
-    return patient && patient.fullName ? patient.fullName : '';
+  selectPatient(patient: any) {
+    this.selectPatient = patient.id;
+    this.searchTerm=patient.name;
+    this.filteredPatients = [];
   }
 }
